63b595d4892e7241e8777ceb4138c32598cc8152
IceSeed <tkm0z@yahoo.co.jp>
2021-06-26 16:32:10 +0900

    single only
    only

--- dl_main.py
+++ dl_main.py
@@ -3,10 +3,10 @@ from tkinter import StringVar
 from tkinter import ttk
 from tkinter.constants import BOTTOM, CENTER, LEFT, TOP
 
-import single, playlist
+import list
 
 class Application( tk.Frame ):
-    def __init__( self, master, title="Youtube_Download  Ver 2.3", width=300, height=150 ):
+    def __init__( self, master, title="Youtube_Download  Ver 2.0", width=300, height=150 ):
         super().__init__( master )
         self.pack()
 
@@ -22,49 +22,31 @@ class Application( tk.Frame ):
         self.youtube_url = ttk.Entry( master=self.master, width=250, text="https://www.youtube.com/watch?v=vjj16qog4vQ" )
         self.youtube_url.pack( side=TOP )
 
-        self.playlist_or_single()
         self.radio()
 
         self.run = ttk.Button( master=self.master, text="Run", command=self.callBack )
         self.run.pack( side=BOTTOM )
 
-    def playlist_or_single( self ):
-        
-        self.str_var_play = StringVar( value="single" )
-
-        self.single = ttk.Radiobutton( master=self.master, text="single", value="single", variable=self.str_var_play )
-        self.single.place( x=50, y=55 )
-        self.playlist = ttk.Radiobutton( master=self.master, text="playlist", value="playlist", variable=self.str_var_play )
-        self.playlist.place( x=180, y=55 )
-        
     def radio( self ):
         self.str_var = StringVar( value="all" )
 
         self.all = ttk.Radiobutton( master=self.master, text="all", value="all", variable=self.str_var )
-        self.all.place( x=20, y=90 )
+        self.all.place( x=20, y=70 )
         self.movie = ttk.Radiobutton( master=self.master, text="movie", value="movie", variable=self.str_var )
-        self.movie.place( x=110, y=90 )
+        self.movie.place( x=110, y=70 )
         self.audio = ttk.Radiobutton( master=self.master, text="audio", value="audio", variable=self.str_var )
-        self.audio.place( x=220, y=90 )
+        self.audio.place( x=220, y=70 )
 
     def callBack( self ):
         #self.youtube_url.get()
         youtube_url = self.youtube_url.get()
         radio_get = self.str_var.get()
-
-        list_or_single = self.str_var_play.get()
-        if list_or_single == "single":
-            self.new_window( youtube_url, radio_get )
-        else:
-            self.new_playlist( youtube_url, radio_get )
-
-    def new_playlist( self, url, mode ):
-        self.newWindow = tk.Toplevel( self.master )
-        self.app2 = playlist.List( self.newWindow, url, mode )
-
+        
+        self.new_window( youtube_url, radio_get )
+        
     def new_window( self, data, mode ):
         self.newWindow = tk.Toplevel( self.master )
-        self.app2 = single.Single( self.newWindow, data, mode )
+        self.app2 = list.List( self.newWindow, data, mode )
 
 def main():
     root = tk.Tk()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import ffmpeg
 
 youtube_url = "https://www.youtube.com/watch?v=vjj16qog4vQ"
 
-#youtube = pytube.YouTube( youtube_url )
+youtube = pytube.YouTube( youtube_url )
 
 #youtube.streams.filter( file_extension='mp4', only_audio=True )
 #dl_title = youtube.title.replace( ".", "" )
@@ -24,21 +24,11 @@ youtube_url = "https://www.youtube.com/watch?v=vjj16qog4vQ"
 
 #os.remove( dl_title + ".mp4" )
 
-#C:\Program Files\Tesseract-OCR;C:\MinGW\bin;C:\Users\Ballista\AppData\Local\Programs\Python\Python36\Scripts\;C:\Users\Ballista\AppData\Local\Programs\Python\Python36\;C:\ffmpeg-4.3.1\bin;C:\Program Files (x86)\MeCab\bin;C:\mysql\bin\;C:\Users\Ballista\AppData\Local\Programs\Python\Python38\Scripts\;C:\Users\Ballista\AppData\Local\Programs\Python\Python38\;C:\Users\Ballista\AppData\Local\atom\bin;C:\Program Files\Java\jdk-12.0.2;C:\mysql\bin;C:\xampp\php\;C:\Users\Ballista\composer;C:\Users\Ballista\AppData\Roaming\Composer\vendor\bin;C:\Program Files\heroku\bin;C:\Users\Ballista\AppData\Roaming\npm;C:\Users\Ballista\AppData\Local\GitHubDesktop\bin;C:\Users\Ballista\AppData\Local\Programs\Microsoft VS Code\bin
+
 
 #format_list = pytube.YouTube( youtube_url ).streams.all()
 #for format in format_list:
 #    print( format.i )
 
-#for item in youtube.streams.filter( file_extension='mp4', only_audio=True ).all():
-#    print( item.itag )
-
-
-from pytube import Playlist
-
-# プレイリストのURLを入れてプレイリストを取得
-p = Playlist( "https://youtube.com/playlist?list=PLHCf3kYa8R_n7yxq54HlKWwbLu0F8R0_-" )
-p.length
-for video in p.videos:
-    print( video.title )
-     #video.streams.first().download()
+for item in youtube.streams.filter( file_extension='mp4', only_audio=True ).all():
+    print( item.itag )
